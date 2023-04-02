92631ed4cbe055c467526c6704253d244d4d9184
jhemesferreira <fs.jhemes@gmail.com>
2023-04-02 10:09:08 +0200

    adding service cross-component communication

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,22 +1,20 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
-export class RecipeListComponent {
-  recipes: Recipe[] = [
-    new Recipe('A tes Recipe', 'this is simply a test',
-    'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'),
-    new Recipe('Another recipe', 'this is simply a test',
-    'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505')
-  ];
+export class RecipeListComponent implements OnInit {
 
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
-  onRecipeSelected(recipe: Recipe){
-    this.recipeWasSelected.emit(recipe);
+  constructor(private recipeService: RecipeService){}
+
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
+
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,19 @@
+import { EventEmitter } from "@angular/core";
+import { Recipe } from "./recipe.model";
+
+export class RecipeService {
+
+  private recipes: Recipe[] = [
+    new Recipe('A tes Recipe', 'this is simply a test',
+    'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'),
+    new Recipe('Another recipe', 'this is simply a test',
+    'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505')
+  ];
+
+  recipeSelected = new EventEmitter<Recipe>;
+
+  getRecipes(): null[]|Recipe[] {
+    return this.recipes.slice();
+  }
+
+}
