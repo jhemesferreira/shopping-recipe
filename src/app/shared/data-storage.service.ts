c8e955a8ff11c1f83fd8524d4145109f0fce84af
jhemesferreira <fs.jhemes@gmail.com>
2023-04-22 10:59:09 +0200

    store data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
 
 // Injectable is optinal theoretically => But we need to add as soon as a service gets another service injected
 @Injectable({
@@ -7,5 +9,17 @@ import { Injectable } from "@angular/core";
 })
 export class DataStorageService
 {
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient, private recipeService: RecipeService){}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+
+    this.http.put<Recipe[]>(
+        'https://angular-course-33335-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+        recipes
+      )
+      .subscribe(response => {
+      console.log(response)
+    });
+  }
 }
